b2967df798708a42b35ad5d20be502c6c44634f5
U-EDU-KCT\k16197yy <k16197yy@IIE19.edu-kct.local>
2019-04-19 10:30:28 +0900

    20190419

---
+++ client.c
@@ -0,0 +1,17 @@
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+int main(int argc,char **argv){
+	
+	int sock=socket(PF_INET,SOCK_STREAM,0);
+	struct sockaddr_in target;
+	target.sin_family=AF_INET;
+	target.sin_addr.s_addr=inet_addr("10.13.64.20");
+	target.sin_port=htons(10001);
+	
+	connect(sock,(struct sockaddr*)&target,sizeof(target));
+	
+	close(sock);
+	return 0;
+}
+
