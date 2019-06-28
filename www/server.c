a2ac292bb54f142c5a76e1c4b8d5ba04da7e8324
U-EDU-KCT\k16197yy <k16197yy@IIE19.edu-kct.local>
2019-06-28 10:25:57 +0900

    20190628

--- www/server.c
+++ www/server.c
@@ -13,15 +13,25 @@ void DieWithError(char *errorMessage){
 void common(int sock){
 	char buf[BUF_SIZE]; 
 	int len_r;
-	if((len_r=recv(sock,buf,BUF_SIZE,0))<=0)
-		DieWithError("recv()failed");
-	buf[len_r]='\0';
-	printf("%s\n",buf);
-	if(send(sock,buf,strlen(buf),0)!=strlen(buf))
-		DieWithError("send()sent a message of unexpected bytes");
+	char response[BUF_SIZE];
+	while((len_r=recv(sock,buf,BUF_SIZE,0))>0){
+		buf[len_r]='\0';
+		printf("%s\n",buf);
+		if(strstr(buf,"\r\n\r\n"){
+			break;
+		}
+	}
+	if(len_r<=0){
+	DieWithError("recv()failed");
+	}
+	printf("recived HTTP request.\n");
+	
+		sprintf(response,"HTTP/1.1 200 OK\r\n");
+		if(sock,response,strlen(response),0!=){
+			
+		}
+	
 }
-
-
 
 int main(int argc,char **argv){
 	int servSock=socket(PF_INET,SOCK_STREAM,0);
@@ -37,7 +47,7 @@ int main(int argc,char **argv){
 	while(1){
 		szClientAddr=sizeof(clientAddress);
 		cliSock=accept(servSock,(struct sockaddr*)&clientAddress,&szClientAddr);
-		common(cliSock);
+			common(cliSock);
 		close(cliSock);
 	}
 	close(servSock);
@@ -45,4 +55,4 @@ int main(int argc,char **argv){
 	return 0;
 }
 
-
+//10.13.64.39
