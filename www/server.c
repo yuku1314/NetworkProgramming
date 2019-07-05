c46e4d5800a273f14c14a0f210151d94817c504e
U-EDU-KCT\k16197yy <k16197yy@IIE19.edu-kct.local>
2019-07-05 09:59:39 +0900

    20190705

--- www/server.c
+++ www/server.c
@@ -12,33 +12,61 @@ void DieWithError(char *errorMessage){
 }
 void common(int sock){
 	char buf[BUF_SIZE]; 
+	char buf_old[BUF_SIZE]; 
+	char buf2[2*BUF_SIZE]; 
 	int len_r;
 	char response[BUF_SIZE];
+	buf_old[0]='\0'; 
 	while((len_r=recv(sock,buf,BUF_SIZE,0))>0){
+		//printf("%s\n",buf); //←リクエストを表示
 		buf[len_r]='\0';
-		printf("%s\n",buf);
-		if(strstr(buf,"\r\n\r\n"){
+		sprintf(buf2,"%s%s",buf_old,buf);
+		if(strstr(buf2,"\r\n\r\n")){
 			break;
 		}
+		sprintf(buf_old,"%s",buf);
 	}
 	if(len_r<=0){
 	DieWithError("recv()failed");
 	}
-	printf("recived HTTP request.\n");
+	printf("recived HTTP request.\n");//受け取ったことを表示
 	
-		sprintf(response,"HTTP/1.1 200 OK\r\n");
-		if(sock,response,strlen(response),0!=){
-			
-		}
+	sprintf(response,"HTTP/1.1 200 OK\r\n");
+	if(send(sock, response, strlen(response), 0) != strlen(response))
+	DieWithError("send() sent a message of unexpected bytes");
+    
+    sprintf(response, "Content-Type: text/html; charset=utf-8\r\n");
+    if(send(sock, response, strlen(response), 0) != strlen(response))
+        DieWithError("send() sent a message of unexpected bytes");
+        
+    sprintf(response, "\r\n");
+    if(send(sock, response, strlen(response), 0) != strlen(response))
+        DieWithError("send() sent a message of unexpected bytes");
+    
+    sprintf(response, "<!DOCTYPE html><html><head><title>");
+    if(send(sock, response, strlen(response), 0) != strlen(response))
+        DieWithError("send() sent a message of unexpected bytes");
+    
+    sprintf(response, "ネットワークプログラミングのwebサイト");
+    if(send(sock, response, strlen(response), 0) != strlen(response))
+        DieWithError("send() sent a message of unexpected bytes");
+    
+    sprintf(response, "</title></head><body>ネットワークダイスキ</body></html>");
+    if(send(sock, response, strlen(response), 0) != strlen(response))
+        DieWithError("send() sent a message of unexpected bytes");
 	
 }
 
 int main(int argc,char **argv){
-	int servSock=socket(PF_INET,SOCK_STREAM,0);
-	int cliSock;
-	unsigned int szClientAddr;
-	struct sockaddr_in servAddress;
 	struct sockaddr_in clientAddress;
+	unsigned int szClientAddr;
+	int cliSock;
+	int servSock=socket(PF_INET,SOCK_STREAM,0);
+	if (servSock < 0){
+        DieWithError("socket() failed");
+	}
+	
+	struct sockaddr_in servAddress;
 	servAddress.sin_family=AF_INET;
 	servAddress.sin_addr.s_addr=htonl(INADDR_ANY);
 	servAddress.sin_port=htons(80);
