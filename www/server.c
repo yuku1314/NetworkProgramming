e8c9a7e0be3c6c1595b2289e2d411a74081a03f1
4431_安野 裕貴 <k16197yy>
2019-07-19 10:27:27 +0900

    20190719

--- www/server.c
+++ www/server.c
@@ -4,6 +4,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <regex.h>
+
 #define BUF_SIZE 256
 
 void DieWithError(char *errorMessage){
@@ -16,15 +18,43 @@ void common(int sock){
 	char buf2[2*BUF_SIZE]; 
 	int len_r;
 	char response[BUF_SIZE];
+	regex_t regBuf;
+	regmatch_t regMatch[1];
 	buf_old[0]='\0'; 
+	char *uri;
+	char result[100];	
+	result[0]='\0';
+	
+	const char *pattern="GET[^\\n]+HTTP";
+	
+	if(regcomp(&regBuf,pattern,REG_EXTENDED | REG_NEWLINE)!=0){
+		DieWithError("regcomp failed");
+	}
+	
 	while((len_r=recv(sock,buf,BUF_SIZE,0))>0){
-		//printf("%s\n",buf); //←リクエストを表示
 		buf[len_r]='\0';
 		sprintf(buf2,"%s%s",buf_old,buf);
+		printf("%s\n",buf); //←リクエストを表示
+		
+		if(regexec(&regBuf,buf2,1,regMatch,0)!=0){
+			int startIndex=regMatch[0].rm_so;
+			int endIndex=regMatch[0].rm_eo;
+			strncpy(result,buf2+startIndex,endIndex-startIndex);
+		}
+		
 		if(strstr(buf2,"\r\n\r\n")){
 			break;
 		}
 		sprintf(buf_old,"%s",buf);
+	}
+	regfree(&regBuf);
+	
+	if (result[0] != '\0') {
+		uri = strtok(result, " ");
+		uri = strtok(NULL, " ");
+		printf("%s\n",uri);
+	} else {
+		DieWithError("No URI");
 	}
 	if(len_r<=0){
 	DieWithError("recv()failed");
@@ -56,6 +86,7 @@ void common(int sock){
         DieWithError("send() sent a message of unexpected bytes");
 	
 }
+
 
 int main(int argc,char **argv){
 	struct sockaddr_in clientAddress;
