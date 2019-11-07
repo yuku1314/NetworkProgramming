df4ae726f6d5457f6bae387c7dc6809fa506d252
yuku1314 <k16197yy@apps.kct.ac.jp>
2019-11-07 16:03:09 +0900

    2019/11/07

--- 3-Message/client.c
+++ 3-Message/client.c
@@ -14,6 +14,12 @@ int prepare_client_socket(char *, int);
 void my_scanf(char *, int);
 void commun(int);
 void read_until_delim(int, char *, char, int);
+void read_certain_bytes(int,void*,int);
+
+struct money{
+    int deposit;
+    int withdraw;
+};
 
 int main(int argc, char *argv[]) {
     // 実行時にはサーバのIPアドレスとポートを添えなければならない
@@ -89,10 +95,10 @@ void read_until_delim(int sock, char *buf, char delimiter, int max_length) {
 }
 
 void commun(int sock) {
-    char cmd[2] = "";                   // コマンド入力用
-    char withdraw[MONEY_DIGIT_SIZE+1];  // 引き出し額
-    char deposit[MONEY_DIGIT_SIZE+1];   // 預け入れ額
-    char msg[BUF_SIZE];                 // 送信メッセージ
+    char cmd[2] = "";               // コマンド入力用
+    struct money msgMoney;          // 引き出し額,預け入れ額 
+    char money[BUF_SIZE];           // 送信メッセージ
+    int result;
     
     printf("0:引き出し　1:預け入れ　2:残高照会　9:終了\n");
     printf("何をしますか？ > ");
@@ -103,20 +109,21 @@ void commun(int sock) {
         case '0':
             // 引き出し処理
             printf("引き出す金額を入力してください > ");
-            my_scanf(withdraw, MONEY_DIGIT_SIZE);
-
-            sprintf(msg, "0_%s_", withdraw);
+            my_scanf(money, MONEY_DIGIT_SIZE);
+            msgMoney.deposit=0;
+            msgMoney.withdraw=atoi(money);
             break;
         case '1':
             //預け入れ処理
             printf("預け入れる金額を入力してください > ");
-            my_scanf(deposit, MONEY_DIGIT_SIZE);
-            
-            sprintf(msg, "%s_0_", deposit);
+            my_scanf(money, MONEY_DIGIT_SIZE);
+            msgMoney.deposit=atoi(money);
+            msgMoney.withdraw=0;
             break;
         case '2':
             // 残高照会
-            strcpy(msg, "0_0_");
+            msgMoney.deposit=0;
+            msgMoney.withdraw=0;
             break;
         default:
             // 終了
@@ -124,13 +131,24 @@ void commun(int sock) {
             return;
     }
 
-    printf("%lu バイト\n",sizeof(char)*strlen(msg));
+    printf("%lu バイト\n",sizeof(msgMoney));
     // 送信処理
-    if(send(sock, msg, strlen(msg), 0) != strlen(msg))
+    if(send(sock, &msgMoney, sizeof(msgMoney), 0) != sizeof(msgMoney))
         DieWithError("send() sent a message of unexpected bytes");
 
     // 受信処理
-    read_until_delim(sock, msg, '_', BUF_SIZE);
+    read_certain_bytes(sock,&result,(int)sizeof(int));
     // 表示処理
-    printf("残高は%d円になりました", atoi(msg));
+    printf("残高は%d円になりました", result);
+}
+
+void read_certain_bytes(int sock,void *buf,int length){
+	int len_r = 0;
+	int len_sum = 0;
+
+	while(len_sum < length){
+		if((len_r= recv(sock,buf+len_sum,length-len_sum,0)) <=0)
+			DieWithError("recv() failed");
+		len_sum += len_r;
+	}
 }
--- 3-Message/sever.c
+++ 3-Message/sever.c
@@ -15,6 +15,12 @@ void commun(int);
 void read_until_delim(int, char *, char, int);
 int get_current_balance();
 void set_current_balance(int);
+void read_certain_bytes(int,void*,int);
+
+struct money{
+	int deposit;
+	int withdraw;
+};
 
 int main(int argc, char *argv[]) {
 	// クライアントの情報を格納するための変数
@@ -105,23 +111,30 @@ void set_current_balance(int new_balance) {
 	return;
 }
 
-void commun(int sock) {
-	char buf[BUF_SIZE];						// 通信用バッファ
+void commun(int sock) {						
 	int balance = get_current_balance(); 	// 預金残高
+	struct money msgMoney;   //受信した金額
 
-	// 区切り文字が出るまでソケットから受信→預け入れ金額をプラス
-	read_until_delim(sock, buf, '_', BUF_SIZE);
-	balance += atoi(buf);
-
-	// 区切り文字が出るまでソケットから受信→引き出し金額をマイナス
-	read_until_delim(sock, buf, '_', BUF_SIZE);
-	balance -= atoi(buf);
+	// 引き出し・預け入れの金額を受信
+	read_certain_bytes(sock, &msgMoney, (int)sizeof(msgMoney));
+	balance += msgMoney.deposit;
+	balance -= msgMoney.withdraw;
 
 	// データベースの預金残高を更新
 	set_current_balance(balance);
 
 	// クライアントへ残高を送信
-	sprintf(buf, "%d_", balance);
-	if(send(sock, buf, strlen(buf), 0) != strlen(buf))
+	if(send(sock, &balance, sizeof(balance), 0) != sizeof(balance))
 		DieWithError("send() sent a message of unexpected bytes");
+}
+
+void read_certain_bytes(int sock,void *buf,int length){
+	int len_r = 0;
+	int len_sum = 0;
+
+	while(len_sum < length){
+		if((len_r= recv(sock,buf+len_sum,length-len_sum,0)) <=0)
+			DieWithError("recv() failed");
+		len_sum += len_r;
+	}
 }
